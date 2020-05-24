7720dee67810f5f7722f7276200b482cb965c22c
GeordiD <geordi.dosher@gmail.com>
2020-05-23 23:40:16 -0500

    Initial Version
    Features:
    + Yes/Maybe/No
    + Use geolocation to get user data
    Todo
    - Add way for user to manually specify location
    - Make prettier

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,136 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div v-if="error">
+      <div v-if="geolocationError">
+        Right now, this only works if you share your location.
+      </div>
+      <div v-else>
+        Well, something broke. I'll try and fix it hang tight.
+      </div>
+    </div>
+    <div v-else-if="isLoading">
+       {{getLoadingMessage()}}
+    </div>
+    <div v-else> 
+      <div id="are-there" >
+        <h1>{{areThere}}</h1>
+        <div v-if="!isWarning && !isWatch">
+          <br/>
+          <h4>{{headline}}</h4>
+          <p>{{description}}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      isWarning: false,
+      isWatch: false,
+      headline: null,
+      description: null,
+      error: false,
+      geolocationError: false,
+      isLoading: true,
+      loadingMessages: [
+        'Hacking NASA',
+        'Polling the guvment',
+        'Reading the Scrying Stones',
+        'Licking finger, holding it up to the sky, and waiting',
+        'Just a sec, lemme check from my porch',
+        'Smelling the air',
+        'Making you wait for no reason, suspense is fun',
+        'Calling memaw',
+        'Burning my essential oils',
+        'Reading the tarrot cards'
+      ]
+    }
+  },
+  computed: {
+    areThere: function() {
+      if (this.isWarning) {
+        return "Yes.";
+      } 
+      else if (this.isWatch) {
+        return "Maybe.";
+      }
+      else {
+        return "No.";
+      }
+    }
+  },
+  methods: {
+    getLoadingMessage: function() {
+      let rand = Math.floor(Math.random() * this.loadingMessages.length);
+      return this.loadingMessages[rand];
+    },
+    checkIfNaders(long, lat) {
+      axios.get('https://api.weather.gov/alerts?event=Tornado Warning&active=true&point=' + lat + ',' + long)
+      .then((res) => {
+          if(res.data && res.data.features && res.data.features.length > 0) {
+            this.isWarning = true;
+            this.isWatch = false;
+            this.headline = res.data.features[0].properties.headline;
+            this.description = res.data.features[0].properties.description;
+            this.isLoading = false;
+          } else {
+            axios.get('https://api.weather.gov/alerts?event=Tornado Watcg&active=true&point=' + lat + ',' + long)
+              .then((res) => {
+                if(res.data && res.data.features && res.data.features.length > 0) {
+                  this.isWarning = false;
+                  this.isWatch = true;
+                  this.headline = res.data.features[0].properties.headline;
+                  this.description = res.data.features[0].properties.description;
+                }
+              })
+              .catch(err => {
+                this.error = true;
+                console.error(err);
+              })
+              .finally(() => {
+                this.isLoading = false;
+              })
+          }
+        })
+      .catch(err => {
+        this.error = true;
+        console.error(err);
+      });
+    }
+  },
+  mounted() {
+    let long = -101.51;
+    let lat = 33.04;
+
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          long = position.coords.longitude;
+          lat = position.coords.latitude;
+          console.log(lat + "," + long);
+          this.checkIfNaders(long, lat);
+        },
+        () => {
+          // we'll have to get it some other way
+          this.error = this.geolocationError = true;
+        }
+      )
+    }
+
+     
   }
 }
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
